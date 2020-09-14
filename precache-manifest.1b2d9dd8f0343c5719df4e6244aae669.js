5c7b2d271034856943e95de6d88c1c8142ad6e75
Dmytro <ddemkovych4@gmail.com>
2020-09-14 17:45:46 +0300

    Updates

---
+++ precache-manifest.1b2d9dd8f0343c5719df4e6244aae669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a15839823a4aab55d128ddbf8f6a62",
+    "url": "/react-intro/index.html"
+  },
+  {
+    "revision": "6dfa4f7e6516b9087c8f",
+    "url": "/react-intro/static/css/main.723869eb.chunk.css"
+  },
+  {
+    "revision": "a56e8a6420c81489a806",
+    "url": "/react-intro/static/js/2.2eb3fee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-intro/static/js/2.2eb3fee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dfa4f7e6516b9087c8f",
+    "url": "/react-intro/static/js/main.661cbc17.chunk.js"
+  },
+  {
+    "revision": "34841bcb7a7d79b90fc4",
+    "url": "/react-intro/static/js/runtime-main.12375e86.js"
+  }
+]);
